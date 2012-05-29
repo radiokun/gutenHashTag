12a2ac54705727db2af2508f4b0d9a41a9474409
daylight <radiolife@me.com>
2012-05-29 23:05:01 +0900

    spwaida

--- generate.php
+++ generate.php
@@ -7,5 +7,78 @@ include('class/map.php');
 
 // Map
 
+<<<<<<< HEAD
 $map = new Map;
 $map->output();
+=======
+// Cities
+
+$cities = City::get('cities.txt');
+
+// Img
+
+$img = imagecreatetruecolor(300, ceil(count($cities) / 2) * 46 + 6);
+
+// Background
+
+$bg = imagecolorallocate($img, 10, 10, 10);
+
+imagefill($img, 0, 0, $bg);
+
+// Font
+
+$bold_font = 'OpenSans-Bold.ttf';
+$light_font = 'OpenSans-Light.ttf';
+
+$m_size = 10;
+$c_size = 14;
+
+// Text
+
+$i = 0;
+$x = 0;
+$y = 0;
+
+foreach($cities as $city)
+{
+	$light = $city->get_light();
+
+	$w = 75 + (180 * $light);
+	$g = 75 + (150 * $light);
+
+	$white = imagecolorallocate($img, $w, $w, $w);
+	$gray = imagecolorallocate($img, $g, $g, $g);
+	$dark = imagecolorallocate($img, 75, 75, 75);
+
+	if($i && $i % 2 == 0)
+	{
+		$x = 0;
+		$y += 46;
+	}
+	imagettftext($img, $m_size, 0, 10 + $x, 20 + $y, $gray, "fonts/{$city->font}", $city->get_message());
+	imagettftext($img, $c_size, 0, 10 + $x, 40 + $y, $dark, "fonts/$light_font", '#');
+	imagettftext($img, $c_size, 0, 22 + $x, 40 + $y, $white, "fonts/$bold_font", $city->name);
+
+	$i++;
+	$x += 140;
+}
+
+// Save
+
+if(is_dir('exports') || mkdir('exports'))
+{
+	imagejpeg($img, 'exports/' . time() . '.jpg', 100);
+}
+
+// Header
+
+header('Content-Type: image/jpeg');
+
+// Print
+
+imagejpeg($img, null, 100);
+
+// Destroy
+
+imagedestroy($img);
+>>>>>>> parent of 6df1bf4... added upload script, with oAuth lib
